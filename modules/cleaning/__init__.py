9236ffc67a005618ccb4df98b4a3335bc74991d3
Lev Lymarenko <sevenzing@gitlab.com>
2020-08-15 12:04:08 +0300

    replace sleep from check_time to forever function

--- modules/cleaning/__init__.py
+++ modules/cleaning/__init__.py
@@ -1,18 +1,22 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher.filters import Command, StateFilter
-from asyncio import AbstractEventLoop
+from asyncio import AbstractEventLoop, sleep as async_sleep
 import logging
 
 from .checker import check_time
+from modules.common.constants import BOT_ADMIN, REMINDER_CHECKER_INTERVAL
 
 
-
-async def forever(function, *args, **kwargs):
+async def forever(function, interval, *args, **kwargs):
     while 1:
         logging.debug(f"Run function {function.__name__}")
         await function(*args, **kwargs)
-
+        await async_sleep(interval)
 
 def setup(dp: Dispatcher, loop: AbstractEventLoop = None, *args, **kwargs):
     logging.debug('Initialize cleaning')
-    loop.create_task(forever(check_time, dp=dp))
+    loop.create_task(forever(
+        function=check_time, 
+        interval=REMINDER_CHECKER_INTERVAL,
+        dp=dp
+        ))
