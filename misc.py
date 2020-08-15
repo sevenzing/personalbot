224f64e828927ff3febc5117c5bc36ad94a84d4d
Lev Lymarenko <sevenzing@gitlab.com>
2020-08-15 17:26:04 +0300

    rename config to constant

--- misc.py
+++ misc.py
@@ -1,19 +1,20 @@
-import modules.common.constants as config
-
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
-
-import logging
 from redis import Redis
+import logging
+
+from modules.common import constants
+
 
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -15s %(funcName) -20s: %(message)s')
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
-database = Redis(**config.redis)
+database = Redis(**constants.redis)
 
-storage = RedisStorage2(**config.redis)
-bot = Bot(token=config.BOT_TOKEN)
+storage = RedisStorage2(**constants.redis)
+bot = Bot(token=constants.BOT_TOKEN)
 dp = Dispatcher(bot, storage=storage)
 
 reminder_loop = asyncio.get_event_loop()
+
