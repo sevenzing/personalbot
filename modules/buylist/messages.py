f1b9b7d2d23684d61bb9b2bdb52f9dfb1eecff9b
Lev Lymarenko <sevenzing@gitlab.com>
2020-08-17 16:26:01 +0300

    fix typo

--- modules/buylist/messages.py
+++ modules/buylist/messages.py
@@ -4,7 +4,7 @@ ADDED_TO_LIST = 'Acknowledged. Look at your list: /list'
 
 SOMETHING_WENT_WRONG = 'Something went wrong. Please, retry the same question in several minutes'
 
-ON_CMD_ADD = 'Send me a list separated by either commas or new lines'
+ON_CMD_ADD = 'Send me a list separated either by commas or new lines'
 
 QUERY_DONE = 'Done'
 
