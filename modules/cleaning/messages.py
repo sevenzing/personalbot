752792f16775aa52845b5ad5665d44aa80a83531
Lev Lymarenko <sevenzing@gitlab.com>
2020-08-24 18:18:11 +0300

    comments for messages

--- modules/cleaning/messages.py
+++ modules/cleaning/messages.py
@@ -1,9 +1,12 @@
 CHANGE_BUILDING_MESSAGE = '🏣 Please, select your building\n'
 
+# | Number of building
 QUERY_SELECTED = 'The %s building selected'
 
+# | 'today' or 'tomorrow'  
 CLEANING_NOTIFICATION = 'Hello! Do not forget about cleaning day %s'
 
+# | Number of building, days left, day number, month name
 NEXT_CLEANING = "Your building is %s, so the next cleaning day will be in *%d days*, on the *%s of %s*.\n"
 
 HAVE_NOT_BUILDING = "You haven't chosen a building yet. \n/setbuilding"
