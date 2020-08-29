07c2bfed2b1db6f45f24728ca7b77d434dbfe649
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-08-29 22:51:09 +0300

    sleep 5 second before restart

--- modules/admin/restart.py
+++ modules/admin/restart.py
@@ -2,6 +2,7 @@ from aiogram import types
 from modules.admin.utils import admin_only_handler, send_message_to_admin
 from modules.common.constants import EXIT_CODES
 from misc import dp
+import asyncio
 import logging
 
 
@@ -11,13 +12,14 @@ async def cmd_restart(message: types.Message, *args, **kwargs):
     Exit from the program if sender is admin
     '''
     logging.warning(f"Admin requests for bot restarting. Stop polling")
-    await message.answer('Stop polling')
+    await send_message_to_admin('Stop polling')
     
     dp.stop_polling()
+    await asyncio.sleep(5)
     await dp.wait_closed()
     
     exit_code = EXIT_CODES['Restart']
     logging.warning(f"Exiting with exit code {exit_code}")
-    await message.answer('Restarting...')
+    await send_message_to_admin('Restarting...')
 
     exit(exit_code)
