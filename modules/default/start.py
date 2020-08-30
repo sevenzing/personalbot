7590312554f3e783306cd50a6ac17697d0bed91b
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-08-30 13:03:37 +0300

    change logic at first meeting

--- modules/default/start.py
+++ modules/default/start.py
@@ -1,7 +1,9 @@
 from aiogram import types
 
 from modules.default import messages
-from modules.database.models.User import create_user_if_not_exists
+from modules.database.models.User import create_user_if_not_exists, find_user
+from modules.cleaning.setbuilding import cmd_setbuilding
+from modules.cleaning.setreminder import cmd_setreminder
 
 import logging
 
@@ -9,7 +11,12 @@ async def cmd_start(message: types.Message):
     """
     Conversation's entry point
     """
+
+    first_time = not find_user({'chat_id': message.chat.id})
     user = create_user_if_not_exists(message.chat.id)
     if message.chat.id == message.from_user.id:
         user.update(username=(message.from_user.username or 'None'))
     await message.answer(messages.ON_CMD_START)
+    if first_time:
+        await cmd_setbuilding(message)
+        await cmd_setreminder(message)
