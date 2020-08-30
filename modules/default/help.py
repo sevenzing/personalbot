95f3c4fd919b282b27ffc6e4f51fd485bf29bd3c
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-08-30 12:11:32 +0300

    markdown to help

--- modules/default/help.py
+++ modules/default/help.py
@@ -3,4 +3,4 @@ import logging
 from modules.default import messages
 
 async def cmd_help(message: types.Message):
-    await message.answer(messages.ON_CMD_HELP)
+    await message.answer(messages.ON_CMD_HELP, parse_mode='Markdown')
